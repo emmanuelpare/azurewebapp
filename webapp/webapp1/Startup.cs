fa3982dcb3fa3f32a6397a2cdde5fb64d4ffecbb
epare <emmanuel.pare@gmail.com>
2016-10-13 10:48:19 -0400

    Initial upload

---
+++ webapp/webapp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webapp1.Startup))]
+namespace webapp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
